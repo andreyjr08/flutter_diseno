5944630fc8c1ca662ba4e359dec021a9cf6ede90
andrey <33665357+andreyjr08@users.noreply.github.com>
2021-06-23 18:12:40 -0500

    prueba indicador de carrusel

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:ink_page_indicator/ink_page_indicator.dart';
+import 'package:carousel_slider/carousel_options.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 // ignore_for_file: public_member_api_docs
 
@@ -18,6 +20,15 @@ class MyApp extends StatelessWidget {
   }
 }
 
+final List<String> imgList = [
+  'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
+  'https://images.unsplash.com/photo-1522205408450-add114ad53fe?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=368f45b0888aeb0b7b08e3a1084d3ede&auto=format&fit=crop&w=1950&q=80',
+  'https://images.unsplash.com/photo-1519125323398-675f0ddb6308?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=94a1e718d89ca60a6337a6008341ca50&auto=format&fit=crop&w=1950&q=80',
+  'https://images.unsplash.com/photo-1523205771623-e0faa4d2813d?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=89719a0d55dd05e2deae4120227e6efc&auto=format&fit=crop&w=1953&q=80',
+  'https://images.unsplash.com/photo-1508704019882-f9cf40e475b4?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=8c6e5e3aba713b17aa1fe71ab4f0ae5b&auto=format&fit=crop&w=1352&q=80',
+  'https://images.unsplash.com/photo-1519985176271-adb1088fa94c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a0c8d632e977f94e5d312d9893258f59&auto=format&fit=crop&w=1355&q=80'
+];
+
 class Home extends StatefulWidget {
   const Home();
   @override
@@ -27,8 +38,10 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  int _current = 0;
   final ValueNotifier<double> page = ValueNotifier(0.0);
   PageIndicatorController controller = PageIndicatorController();
+  final CarouselController _controller = CarouselController();
 
   IndicatorShape shape = IndicatorShape(width: 0, height: 0);
   IndicatorShape activeShape = IndicatorShape(height: 0, width: 0);
@@ -36,10 +49,7 @@ class _HomeState extends State<Home> {
   @override
   void initState() {
     super.initState();
-    controller = PageIndicatorController()
-      ..addListener(() {
-        page.value = controller.page!;
-      });
+    controller = _controller as PageIndicatorController;
   }
 
   List<Widget> _createChildren(int count) {
@@ -75,6 +85,36 @@ class _HomeState extends State<Home> {
     );
   }
 
+  final List<Widget> imageSliders = imgList
+      .map((item) => Container(
+            child: Container(
+              // margin: EdgeInsets.all(5.0),
+              child: ClipRRect(
+                  borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                  child: Stack(
+                    children: <Widget>[
+                      Image.network(item, fit: BoxFit.cover, width: 1000.0),
+                      Positioned(
+                        child: Container(
+                          decoration: BoxDecoration(
+                            gradient: LinearGradient(
+                              colors: [
+                                Color.fromARGB(200, 0, 0, 0),
+                                Color.fromARGB(0, 0, 0, 0)
+                              ],
+                              begin: Alignment.bottomCenter,
+                              end: Alignment.topCenter,
+                            ),
+                          ),
+                          padding: EdgeInsets.symmetric(
+                              vertical: 10.0, horizontal: 20.0),
+                        ),
+                      ),
+                    ],
+                  )),
+            ),
+          ))
+      .toList();
   @override
   Widget build(BuildContext context) {
     final children = _createChildren(5);
@@ -84,6 +124,20 @@ class _HomeState extends State<Home> {
 
     return Stack(
       children: <Widget>[
+        CarouselSlider(
+          items: imageSliders,
+          carouselController: _controller,
+          options: CarouselOptions(
+              autoPlay: false,
+              enlargeCenterPage: false,
+              aspectRatio: 3,
+              viewportFraction: 1,
+              onPageChanged: (index, reason) {
+                setState(() {
+                  _current = index;
+                });
+              }),
+        ),
         Column(
           children: <Widget>[
             buildInkPageIndicator(InkStyle.simple),
